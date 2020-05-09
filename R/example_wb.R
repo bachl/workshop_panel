a85270b1db964800d303c7e4530a9b149aa6d7a4
Marko <marko.bachl@gmail.com>
2020-05-09 15:34:51 +0200

    Plot fertig

---
+++ R/example_wb.R
@@ -0,0 +1,21 @@
+## ---- example-wb
+
+set.seed(1383);
+
+n = 8; t = 6; tce = 1
+
+example_data = tibble(id = 1:n,
+                      z = seq(-3, 3, length.out = n),
+                      m_beer = z * 1 + rnorm(n)) %>% 
+  replicate(t, ., simplify = FALSE) %>% 
+  bind_rows() %>% 
+  mutate(beer = m_beer + rnorm(n*t),
+         beer = beer + abs(min(beer)),
+         hangover = beer * tce + z * -2 + rnorm(n*t))
+
+plt_pool = example_data %>% 
+  ggplot(aes(beer, hangover)) + geom_point() + stat_smooth(method = "lm", se = FALSE)
+
+plt_nopool = example_data %>% 
+  ggplot(aes(beer, hangover, color = factor(id))) + geom_point() + stat_smooth(method = "lm", se = FALSE)
+
--- R/packages.R
+++ R/packages.R
@@ -2,6 +2,7 @@
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
+theme_set(theme_bw()) # ggplot theme
 
 tibble(package = c("R", sort(pacman::p_loaded()))) %>% 
   mutate(version = map_chr(package, ~as.character(pacman::p_version(package = .x)))) %>% 
