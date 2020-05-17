c6f2377d4acec9a7ef0925bdec88655a76c609d2
Marko <marko.bachl@gmail.com>
2020-05-17 22:53:11 +0200

    RE including start ICC
    Weiter mit ICC for multiple groups

---
+++ R/icc.R
@@ -0,0 +1,16 @@
+## ---- icc
+
+# Null-Modell
+m0 = d %>%
+  lmer(verh1 ~ 1 + (1 | IDsosci), data = .)
+
+m0 %>% 
+  summary()
+
+# ICC "von Hand"
+round(0.5938 / (0.5938 + 0.4065), 3)
+
+# Mit performance::icc
+icc(m0)
+
+# icc(lmer(verh1 ~ 1 + (1 | IDsosci) + (1 | wave), data = d), by_group = TRUE)
